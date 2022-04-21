b80ab74d850e36844be2e0ec9b45335b278136cc
Masaomi Hatakeyama <masaomi.hatakeyama@gmail.com>
2022-04-21 14:18:20 +0200

    Added product category schema, context

--- lib/hello_web/router.ex
+++ lib/hello_web/router.ex
@@ -18,6 +18,7 @@ defmodule HelloWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    resources "/products", ProductController
     get "/hello", HelloController, :index
     get "/hello/:messenger", HelloController, :show
     get "/search", SearchController, :index
